9a8b8a71a95242ad388b30a6057ccdaa08beb5f4
ics-kano <takeshi.kano@ics-web.jp>
2019-03-27 23:34:02 +0900

    Parcel - TypeScript：ビルドファイルのリファクタリング

--- myproject/src/scripts/index.ts
+++
@@ -1,3 +0,0 @@
-import { myFunction } from "./myFunction";
-const todayWeather = myFunction(27);
-document.querySelector("#weather").textContent = todayWeather;
--- myproject/src/scripts/myFunction.ts
+++
@@ -1,3 +0,0 @@
-export function myFunction(temperature: number): string {
-  return `東京の気温は${temperature}度です`;
-}
