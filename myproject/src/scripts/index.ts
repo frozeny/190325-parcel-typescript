34f091d94d6e1f1ea685936e56e2451f04923190
ics-kano <takeshi.kano@ics-web.jp>
2019-03-28 17:00:23 +0900

    Parcel - TypeScript：サンプルの更新

--- myproject/src/scripts/index.ts
+++ myproject/src/scripts/index.ts
@@ -1,6 +1,12 @@
+// myFunction.tsをインポートする
 import { myFunction } from "./myFunction";
+// 今日の天気用文字列を生成する
 const todayWeather = myFunction(27);
-document.querySelector("#weather").textContent = todayWeather;
+// #weather要素のテキストとして今日の天気を設定する
+document
+  .querySelector("#weather")
+  .textContent
+  = todayWeather;
 
 
 
